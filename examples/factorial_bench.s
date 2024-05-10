d341c7312000d59d1e9a04bb8cd2ba1c57a11518
vvit19 <vitaliysavchenko555@gmail.com>
2024-05-10 21:43:01 +0300

    Fix bug in while and done readme

---
+++ examples/factorial_bench.s
@@ -0,0 +1,156 @@
+%include "stdlib.s" 
+global _start 
+section .data 
+variables: times 8192 db 0 
+section .text 
+_start: 
+		mov rbx, variables 
+		mov rbp, rbx 
+		add rbp, 256 
+		call main 
+		mov eax, 1 
+		mov ebx, 0 
+		int 80h 
+main: 
+		pop rsi 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x4018000000000000 
+		mov qword [rsp], rcx 
+
+		pop qword [rbp + 0] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x0 
+		mov qword [rsp], rcx 
+
+		pop qword [rbp + 8] 
+		push qword [rbp + 8] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x4187d78400000000 
+		mov qword [rsp], rcx 
+
+		pop rcx 
+		pop rdx 
+		cmp rcx, rdx 
+		jle L_0 
+L_1: 
+		push rsi 
+		push qword [rbp + 0] 
+
+; Call: 
+		add rbp, 256 
+		call factorial 
+		sub rbp, 256 
+
+		pop rsi 
+		push rax 
+		pop qword [rbp + 16] 
+		push qword [rbp + 8] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x3ff0000000000000 
+		mov qword [rsp], rcx 
+
+
+; Math operation:
+		movsd xmm0, qword [rsp + 8] 
+		movsd xmm1, qword [rsp] 
+		pop rcx
+		addsd xmm0, xmm1 
+		movsd qword [rsp], xmm0 
+
+		pop qword [rbp + 8] 
+		push qword [rbp + 8] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x4187d78400000000 
+		mov qword [rsp], rcx 
+
+		pop rcx 
+		pop rdx 
+		cmp rcx, rdx 
+		jle L_0 
+		jmp L_1 
+L_0: 
+		push rsi 
+		push qword [rbp + 16] 
+		call out 
+		pop rsi 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x0 
+		mov qword [rsp], rcx 
+
+		pop rax 
+		push rsi 
+		ret 
+factorial: 
+		pop rsi 
+		pop qword [rbp + 0] 
+		push qword [rbp + 0] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x3ff0000000000000 
+		mov qword [rsp], rcx 
+
+		pop rcx 
+		pop rdx 
+		cmp rcx, rdx 
+		jne L_2 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x3ff0000000000000 
+		mov qword [rsp], rcx 
+
+		pop rax 
+		push rsi 
+		ret 
+L_2: 
+		push rsi 
+		push qword [rbp + 0] 
+
+; Push imm (double) 
+		sub rsp, 8 
+		mov rcx, 0x3ff0000000000000 
+		mov qword [rsp], rcx 
+
+
+; Math operation:
+		movsd xmm0, qword [rsp + 8] 
+		movsd xmm1, qword [rsp] 
+		pop rcx
+		subsd xmm0, xmm1 
+		movsd qword [rsp], xmm0 
+
+
+; Call: 
+		add rbp, 256 
+		call factorial 
+		sub rbp, 256 
+
+		pop rsi 
+		push rax 
+		pop qword [rbp + 8] 
+		push qword [rbp + 0] 
+		push qword [rbp + 8] 
+
+; Math operation:
+		movsd xmm0, qword [rsp + 8] 
+		movsd xmm1, qword [rsp] 
+		pop rcx
+		mulsd xmm0, xmm1 
+		movsd qword [rsp], xmm0 
+
+		pop rax 
+		push rsi 
+		ret 
